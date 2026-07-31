7af94485ebd2b5e40d781ae35912437ab1894888
Rafal <rafal.z.malucha@gmail.com>
2026-07-31 03:24:00 +0200

    add traffic lights

---
+++ Assets/_Scripts/SingleTrafficLight.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SingleTrafficLight : MonoBehaviour
+{
+    [SerializeField] private bool _isRed = true;
+    [SerializeField] private GameObject _redLightIndicator;
+    [SerializeField] private GameObject _greenLightIndicator;
+    [SerializeField] private GameObject _stopTrigger;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isRed)
+        {
+            HandleLightChangeToRed();
+        }
+        else
+        {
+            HandleLightChangeToGreen();
+        }
+    }
+
+    private void HandleLightChangeToGreen()
+    {
+        _greenLightIndicator.SetActive(true);
+        _redLightIndicator.SetActive(false);
+        _stopTrigger.SetActive(false);
+    }
+
+    private void HandleLightChangeToRed()
+    {
+        _greenLightIndicator.SetActive(false);
+        _redLightIndicator.SetActive(true);
+        _stopTrigger.SetActive(true);
+    }
+
+    public void ChangeLight()
+    {
+        _isRed = !_isRed;
+    }
+}
